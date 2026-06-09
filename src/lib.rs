e11d2456b2b757e50c8641a1af480b9bbb5df86e
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2026-06-09 21:32:09 +0200

    feat: setup initial rendering, event handling and loop

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use anyhow::Result;
 use bevy_ecs::{prelude::*, schedule};
 use crossterm::{
     cursor::{Hide, MoveTo},
+    event::{Event, KeyCode, KeyEvent, poll, read},
     execute, queue,
     style::Print,
     terminal::{
@@ -20,22 +21,22 @@ mod render;
 
 static TITLE: &str = "Termshot";
 
-#[derive(Resource)]
+#[derive(Debug, Resource)]
 struct Exit;
+
+#[derive(Debug, Resource)]
+struct BufferSize {
+    cols: u16,
+    rows: u16,
+}
 
 pub fn app() -> Result<()> {
     let (cols, rows) = setup_terminal()?;
-
-    let (mut world, mut schedule) = setup_bevy();
+    let buffer_size = BufferSize { cols, rows };
+    let (mut world, mut schedule) = setup_bevy(buffer_size);
 
     loop {
         schedule.run(&mut world);
-        // collect Input and write to ressource
-        // collect deltatime and write to ressource
-        // wait a certain amount of time
-        // maybe check for exit resource?
-
-        world.insert_resource::<Exit>(Exit);
 
         if world.get_resource::<Exit>().is_some() {
             clean_up_terminal()?;
@@ -44,11 +45,62 @@ pub fn app() -> Result<()> {
     }
 }
 
-fn setup_bevy() -> (World, Schedule) {
+fn setup_bevy(buffer_size: BufferSize) -> (World, Schedule) {
     let mut world = World::new();
+    world.insert_resource(buffer_size);
+
     let mut schedule = Schedule::default();
 
+    schedule.add_systems(
+        (
+            process_events.pipe(process_event_error_handler),
+            render::render.pipe(render::error_handler_system),
+        )
+            .chain(),
+    );
+
     (world, schedule)
+}
+
+fn process_events(mut buff_size: ResMut<BufferSize>, mut commands: Commands) -> anyhow::Result<()> {
+    while poll(Duration::from_millis(0))? {
+        match read()? {
+            Event::Resize(cols, rows) => {
+                buff_size.cols = cols;
+                buff_size.rows = rows;
+            }
+            Event::FocusGained => {}
+            Event::FocusLost => {}
+            Event::Key(key_event) => process_key_events(key_event, &mut commands),
+            Event::Mouse(_mouse_event) => {}
+            Event::Paste(_) => {}
+        }
+    }
+    Ok(())
+}
+
+fn process_event_error_handler(In(result): In<Result<(), anyhow::Error>>) {
+    if result.is_err() {}
+}
+
+#[expect(unused_variables)]
+fn process_key_events(key_event: KeyEvent, commands: &mut Commands) {
+    match key_event {
+        KeyEvent {
+            code: KeyCode::Char('q'),
+            kind,
+            modifiers,
+            state,
+        } => {
+            commands.insert_resource(Exit);
+        }
+        KeyEvent {
+            code,
+            modifiers,
+            kind,
+            state,
+        } => {}
+    }
 }
 
 fn setup_terminal() -> Result<(u16, u16)> {
@@ -57,7 +109,7 @@ fn setup_terminal() -> Result<(u16, u16)> {
         io::stdout(),
         EnterAlternateScreen,
         SetTitle(TITLE),
-        // Hide,
+        Hide,
         MoveTo(0, 0),
         Clear(crossterm::terminal::ClearType::All)
     )?;
--- src/render.rs
+++ src/render.rs
@@ -1,5 +1,44 @@
-use crossterm::{queue, style::Print};
+use std::io::{Write, stdout};
 
-pub fn render() {
-    queue!(Print("hello there!"));
+use bevy_ecs::{
+    resource::Resource,
+    system::{Commands, In, Res},
+};
+use crossterm::{
+    cursor::MoveTo,
+    queue,
+    style::Print,
+    terminal::{Clear, ClearType},
+};
+
+use crate::{BufferSize, Exit};
+
+pub fn render(buff_size: Res<BufferSize>) -> anyhow::Result<()> {
+    let BufferSize { cols, rows } = buff_size.as_ref();
+
+    let center_col = cols / 2;
+    let center_row = rows / 2;
+
+    let start_col = center_col - 6;
+
+    if *cols >= center_col + 6 {
+        let mut out = stdout().lock();
+        queue!(
+            out,
+            Clear(ClearType::All),
+            MoveTo(0, 0),
+            MoveTo(start_col, center_row),
+            Print("hello there!")
+        )?;
+        out.flush()?;
+    }
+
+    Ok(())
+}
+
+pub fn error_handler_system(In(result): In<Result<(), anyhow::Error>>, mut commands: Commands) {
+    if let Err(e) = result {
+        std::fs::write("crash.log", format!("Render failed: {}", e)).unwrap();
+        commands.insert_resource(Exit);
+    }
 }
